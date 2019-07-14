75b38068c1b56455211e98f31cf33939e61a2574
zhouheng <2609387872@qq.com>
2019-07-14 20:09:23 +0800

    gjh

---
+++ day05exam/src/main/java/day05exam/Test.java
@@ -0,0 +1,13 @@
+package day05exam;
+
+import java.util.Arrays;
+
+public class Test {
+
+		public static void main(String[] args) {
+			System.out.println("hello world");
+			String [] str=[];
+			Arrays.sort(str(s1,s2)->)
+		}
+	 
+}
